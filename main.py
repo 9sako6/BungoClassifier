9a2ddfcc13a7d3e7e152b2f34de4195e6ce58ab2
9sako6 <31821663+9sako6@users.noreply.github.com>
2018-11-01 00:51:03 +0900

    update, add authors

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def text2vec(text, dictionary):
     text = tagger.parse(text)
     words_list = text.split(' ')
     # 単語列をID列に変換する
-    id_list = np.full(50, 88888) # 未知語のIDは88888とする
+    id_list = np.full(50, 109999) # 未知語のIDは109999とする
     for i, word in enumerate(words_list):
         if i >= 50:
             break
@@ -50,9 +50,9 @@ if __name__ == '__main__':
 
     text = input('判定する文章：')
     predictions = model.predict(text2vec(text, dictionary))
-    #vec = [1,639,1297,51,13397,47,10,0,693,30,1110,1598,15,334,10,109,2044,9,1,4292,17,0,4092,1094,25908,13,4349,2606,4240,10,3664,8,97,17,91,8,6,0,16932,3545,16,1616,15,0,25908,15,39494,211,10,0]
-    #doc = vec2text(vec, dictionary)
-    #print(doc)
+    # vec = [1,639,1297,51,13397,47,10,0,693,30,1110,1598,15,334,10,109,2044,9,1,4292,17,0,4092,1094,25908,13,4349,2606,4240,10,3664,8,97,17,91,8,6,0,16932,3545,16,1616,15,0,25908,15,39494,211,10,0]
+    # doc = vec2text(vec, dictionary)
+    # print(doc)
     # 結果表示
     for i, author in enumerate(authors):
-        print('{}度:'.format(author), predictions[0][i])
+        print('{}度:\t'.format(author), round(predictions[0][i], 3))
